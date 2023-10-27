03e05d6a4646abff3c68840ae8d80c7871fb9ea3
Pryann <pryann@gmail.com>
2023-10-27 10:53:57 +0200

    update readme

--- 02-functions/exercises/01/solution/js/main.js
+++
@@ -1,8 +0,0 @@
-const handleClick = () => {
-  const buttons = document.querySelectorAll('.button')
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) =>
-      console.log(event.target.textContent)
-    )
-  }
-}
