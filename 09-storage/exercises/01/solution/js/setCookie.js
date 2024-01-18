0960a8be88c1f006e72885ab3900409451bfe4c1
Chris781231 <ligeti.karoly78@gmail.com>
2024-01-18 15:30:59 +0100

    Add 09-storage exercises solutions.

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,1 +1,9 @@
+const setCookie = (str) => {
+  const expirationMinutes = 15;
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + expirationMinutes);
+  const expires = now.toUTCString();
+  document.cookie = `token=${str}; expires=${expires}; path=/`;
+};
+
 export default setCookie;
--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,1 +1,26 @@
+const cookieHandler = {
+  getAll() {
+    const cookies = document.cookie.split('; ');
+    const result = {};
+    cookies.forEach((cookie) => {
+      const cookieArray = cookie.split('=');
+      // eslint-disable-next-line prefer-destructuring
+      result[cookieArray[0]] = cookieArray[1];
+    });
+    return result;
+  },
+  toSessionStorage() {
+    const cookies = this.getAll();
+    Object.entries(cookies).forEach(([key, value]) => {
+      sessionStorage.setItem(key, value);
+    });
+  },
+  flush() {
+    const cookies = this.getAll();
+    Object.keys(cookies).forEach((key) => {
+      document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    });
+  },
+};
+
 export default cookieHandler;
