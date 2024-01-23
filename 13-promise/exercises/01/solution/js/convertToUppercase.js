ccb23a01678ae7bd100691fbcbafa395d8189f9d
Chris781231 <ligeti.karoly78@gmail.com>
2024-01-23 14:04:23 +0100

    Add 13-promise exercises solutions.

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const convertToUppercase = (arr) => new Promise((resolve, reject) => {
+    try {
+        resolve(arr.map((item) => item.toLocaleUpperCase()));
+    } catch (error) {
+        reject(new Error('Error: Not all items in the array are strings!'));
+    }
+});
+
 export default convertToUppercase;
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const sortStrings = (arr) => new Promise((resolve, reject) => {
+        // setTimeout(() => {
+            const isStringArray = Array.from(arr).every((item) => typeof item === 'string');
+            if (isStringArray) {
+                try {
+                    resolve(arr.sort());
+                } catch (error) {
+                    reject(Error('Error: Something went wrong with sorting words!'));
+                }
+            } else {
+                reject(Error('Error: Not all items in the array are strings!'));
+            }
+        // });
+    });
+
 export default sortStrings;
